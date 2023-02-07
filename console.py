b6f914c1b6262511f4742e42bb39b37cb038364c
BrianKvin <briankvin@gmail.com>
2023-02-07 17:30:16 -0500

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This program contains the entry point of the command interpreter."""
+
+
+import cmd
+
+
+class HBNBCCommand(cmd.Cmd):
+    """Class for the command interpreter"""
