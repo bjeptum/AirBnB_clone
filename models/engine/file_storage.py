6889974774e1afdd831cb40e704da26d8be69610
BrianKvin <briankvin@gmail.com>
2023-02-07 17:08:19 -0500

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,8 @@ Filestorage module
 class FileStorage:
     """Serializes instances to a JSON file and
     deserializes JSON file to instances"""
-    def __init__(self):
+    # Added private class attributes to the __init__ method
+    def __init__(self, __file_path, __objects):
         self.__file_path = __file_path
         self.__objects = __objects
 
@@ -15,5 +16,14 @@ class FileStorage:
         return self. __objects
     
     def new(self, obj):
-        """Sets in __objects"""
-        
+        """Sets in __objects the obj with key <obj class name>.id"""
+        return FileStorage.__objects
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        pass
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists\
+                ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        pass
