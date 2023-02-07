7c7aa62f1c4b7ddf80cfa7db614781249aa7de17
b_jeptum <brendajeptum@gmail.com>
2023-02-07 05:35:38 -0800

    Updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ import datetime
 
 class BaseModel:
     """ Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def i__init__(self, id, created_at, updated_at):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at= datetime.datetime.now()
