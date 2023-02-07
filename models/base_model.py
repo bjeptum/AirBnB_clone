32c12774d63f71a63895e3fd72af8010284f4b15
b_jeptum <brendajeptum@gmail.com>
2023-02-07 03:32:53 -0800

    Second commit update on base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,26 @@
 Base module
 """
 
+import uuid
+import datetime
+
+
 
 class BaseModel:
     """ Defines all common attributes/methods for other classes"""
-    def __init__(self, id = None):
-        self.id = id
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at= datetime.datetime.now()
+
+    def __str__(self):
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+        
 
     def save(self):
-        """Updates insatnce attribute with the current datetime"""
-        pass
+        """Updates instance attribute with the current datetime"""
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """ Returns all keys/values of __dict__ of the instance"""
-        retuen self.__dict__
-
+        return self.__dict__
