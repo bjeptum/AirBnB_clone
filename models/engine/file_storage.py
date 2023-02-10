13b35d375cabc52b672c8f80c9e9ab751413bbaa
BrianKvin <briankvin@gmail.com>
2023-02-10 16:54:38 -0500

    Public instance methods save and reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,17 +26,34 @@ class FileStorage:
     
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        pass
+        # the object type = self.__class__.__name__
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        # Create a new dictionary from the object stored in FileStorage.__objects dict
+        # The new dict contains key-value pairs, where the key is the same as the FileStorage.__objects dict
+        # The result is a dict, with the same key as FileStorage.__objects dict
+        # but with vaues converted to dict
+        my_json_dict = {}
+        for key, value in FileStorage.__objects.items():
+            my_json_dict[key] = value.to_dict()
         with open(FileStorage._file_path, "w", encoding="utf-8") as f:
-            json.dump(f)
+            json.dump(my_json_dict, f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists\
                 ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
-        if not os.path.isfile(FileStorage._file_path):
-            return
-        with open(FileStorage._file__file_path, "r", encoding="utf-8") as f:
+        # if not os.path.isfile(FileStorage._file_path)
+        if (exists(FileStorage.__file_path)):
+            # checks if the file exists, opens it, and loads the JSON data
+            with open(FileStorage._file__file_path, "r", encoding="utf-8") as f:
+                my_obj = json.load(f)
+                # iterate through the values of the JSON and sets class name and values
+                for value in my_obj.values():
+                    class_name = value['__class__name']
+                    del value['__class__']
+                    # remove class key and create a new instance of the class and pass the values as key args
+                    self.new(eval(class_name)(**value))
 
