b867cc0d4d0560195663e4f025ffb76cde41750d
BrianKvin <briankvin@gmail.com>
2023-02-10 07:52:01 -0500

    Update on JSON file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """
 Filestorage module
 """
+import json
 
 
 class FileStorage:
@@ -13,7 +14,7 @@ class FileStorage:
         self.__objects = __objects
 
     def all(self):
-        return self. __objects
+        return self.__objects
     
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
@@ -21,9 +22,13 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        pass
+        with open(FileStorage._file_path, "w", encoding="utf-8") as f:
+            json.dump(f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists\
                 ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
-        pass
+        if not os.path.isfile(FileStorage._file_path):
+            return
+        with open(FileStorage._file__file_path, "r", encoding="utf-8") as f:
+
