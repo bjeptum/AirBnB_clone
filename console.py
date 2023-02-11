057a448f37b9507cc0ab305ab06eb58448cc8ed0
BrianKvin <briankvin@gmail.com>
2023-02-11 08:39:55 -0500

    Instances on console

--- console.py
+++ console.py
@@ -19,13 +19,40 @@ class HBNBCCommand(cmd.Cmd):
         """Exits the program."""
         pass
     
-    def do_create(self, arg):
+    def do_create(self, line):
         """Creates an instance."""
-        pass
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesnt't exist **")
+        else:
+            """If the line is in the storage class() method, create an instance\
+                    of the line and print the id"""
+            new_instance = storage.classes()[line]()
+            new_instance.save()
+            print(new_instance.id)
 
-    def do_show(self, arg):
+    def do_show(self, line):
         """Prints the string representtation of an instance."""
-        pass
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+        line = line.split()
+        if len(line) == 1:
+            print("** instance id missing **")
+            return
+        class_name = line[0]
+        class_id = line[1]
+        if class_name not in model.storage.all():
+            print("** class doesn't exist **")
+            return
+        instances = models.storage.all()[class_name]
+        if class_id not in instances:
+            print("** no instance found **")
+            return
+        print(instances[class_id]
+        """
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
@@ -36,6 +63,7 @@ class HBNBCCommand(cmd.Cmd):
         pass
 
     def do_update(self, arg):
+        pass
 
 
 if __name__ == '__main__':
